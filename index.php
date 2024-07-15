0cb1e3d95a04958487ae2b4aab2d1544b14c3be0
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-07-15 20:33:16 -0300

    Class Sessao

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-require './sistema/configuracao.php';
+// require './sistema/configuracao.php';
 require 'vendor/autoload.php';
-require 'rotas.php';
+// require 'rotas.php';
+
+$sessao=new sistema\Nucleo\Sessao();
+
+$sessao->criar('usuario', [
+    'idade'=>25, 
+    'nome'=>'Wellington']);
+
+var_dump($sessao->carregar());
---
+++ sistema/Nucleo/Sessao.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace sistema\Nucleo;
+
+class Sessao{
+    public function __construct(){
+        if(!session_id()){
+            session_start();
+        }
+    }
+
+    public function criar(string $chave, mixed $valor):Sessao{
+        if(is_array($valor)){
+            $_SESSION[$chave]=(object) $valor;
+        }else{
+            $_SESSION[$chave]=$valor;
+        }
+
+        return $this;
+    }
+
+    public function carregar():?object{
+        return (object) $_SESSION;
+    }
+
+    public function checar(string $chave):bool{
+        return isset($_SESSION[$chave]);
+    }
+
+    public function limpar(string $chave){
+        unset($_SESSION[$chave]);
+        return $this;
+    }
+
+    public function deletar():Sessao{
+        session_destroy();
+        return $this;
+    }
+}
