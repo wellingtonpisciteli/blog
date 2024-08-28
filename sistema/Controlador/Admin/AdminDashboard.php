5fc919f8fa6daa0b0afc8d7097ee61968e131889
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-08-28 20:40:37 -0300

    TESTES

--- sistema/Controlador/Admin/AdminDashboard.php
+++ sistema/Controlador/Admin/AdminDashboard.php
@@ -16,13 +16,13 @@ class AdminDashboard extends AdminControlador{
         $categorias=new CategoriaModelo();
         
         echo($this->template->renderizar('dashboard.html', [
-            'posts'=>$post->busca()->ordem('status ASC, id DESC')->limite(5)->resultado(true),
+            'posts'=>$post->busca()->ordem('status ASC, id DESC')->limite(3)->resultado(true),
             'total'=>[
                 'total'=>$post->busca()->total(),
                 'ativo'=>$post->busca('status=1')->total(),
                 'inativo'=>$post->busca('status=0')->total()
             ],
-            'categorias'=>$categorias->busca()->ordem('status ASC, id DESC')->limite(5)->resultado(true),
+            'categorias'=>$categorias->busca()->ordem('status ASC, id DESC')->limite(4)->resultado(true),
             'total'=>[
                 'total'=>$categorias->busca()->total(),
                 'ativo'=>$categorias->busca('status=1')->total(),
